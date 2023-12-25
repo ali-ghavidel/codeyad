6e9ed0b39b7192b2037000ca2547bbb106e2062f
ali-ghavidel <ghavidel.aliabadi@gmail.com>
2023-12-25 22:52:44 +0330

    part#24

--- src/TaskItems.jsx
+++ src/TaskItems.jsx
@@ -3,24 +3,38 @@ import { TaskContext } from './TaskContext';
 
 const TasksList = () => {
   const {taskItems, setTaskItems} = useContext(TaskContext);
-        return(
-          <ul className='list-group m-0 p-0 mt-2'>
-            {taskItems.map((value) => {
-              return(
-                <li className={`list-group-item d-flex justify-content-between ${value.done ? "list-group-item-success" : "" }`} key={value.id}>
-                  {value.title}
-                  <span>
-                    {value.done ? <i className='me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow'></i>
-                    : <i className='me-3 pointer fas fa-check text-success transition_200 text_hover_shadow'></i> }
-                    
-                    
-                    <i className='me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow'></i>
-                  </span>
-                </li>
-              )
-            })}
-            
-        </ul>
-        )
+
+  const HandleSetIsDone = (id) => {
+    const index = taskItems.findIndex((v)=>v.id === id);
+    let cpyTaskItems = [...taskItems];
+    cpyTaskItems[index].done = !cpyTaskItems[index].done;
+    setTaskItems(cpyTaskItems);
+    console.log(index);
+  }
+  const deleteTaskItems = (id) => {
+    setTaskItems(taskItems.filter(v => v.id !== id));
+  }
+
+  if(taskItems.length){
+    return(
+      <ul className='list-group m-0 p-0 mt-2'>
+        {taskItems.map((value, key) => {
+          return(
+            <li className={`list-group-item d-flex justify-content-between ${value.done ? "list-group-item-success" : "" }`} key={key}>
+              {value.title}
+              <span>
+                <i className={`me-3 pointer fas ${value.done ? "fa-check text-success" : "fa-times text-warning"} transition_200 text_hover_shadow`} onClick={()=>HandleSetIsDone(value.id)}></i>
+                <i className='me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow' onClick={()=>deleteTaskItems(value.id)}></i>
+              </span>
+            </li>
+          )
+        })}
+        
+    </ul>
+    )
+  }else{
+    return <h4 className="text-danger text-center mt-3 mb-3">موردی برای نمایش وجود ندارد</h4>
+  }
+        
 }
 export default TasksList;
