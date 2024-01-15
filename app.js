e072ab88c9eb8548b3c0cef5a9cd6171e4765fd7
Ali Ghavidel <ghavidel.aliabadi@gmail.com>
2024-01-15 09:40:03 +0330

    part#56

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
 
 
-const states = {
-    apple: 5
+const fruitStates = {
+    apple: 5,
+    orange: 10
+}
+const foodStates = {
+    sandwitch: 15
 }
 
-const reducer = (state = states, action) => {
+const fruitReducer = (state = fruitStates, action) => {
     switch (action.type) {
         case "buy-apple":
-            return {apple: state.apple - 1}
+            return {...state, apple: state.apple - 1}
+        case "buy-orange":
+            return {...state, orange: state.orange - 1}
         default:
             return state
     }
 }
 
-const store = Redux.createStore(reducer);
+const foodReducer = (state = foodStates, action) => {
+    switch (action.type) {
+        case "buy-sandwitch":
+            return {...state, sandwitch: state.sandwitch - 1}
+        default:
+            return state
+    }
+}
 
-store.subscribe(()=>{
-    document.getElementById("apples").innerHTML = store.getState().apple;
+const rootReducer = Redux.combineReducers({
+    fruit: fruitReducer,
+    food: foodReducer
 })
 
-document.getElementById("purchase").addEventListener('click', ()=>{
+const store = Redux.createStore(rootReducer);
+
+const render = () => {
+    console.log(store.getState());
+    document.getElementById("apples").innerHTML = store.getState().fruit.apple;
+    document.getElementById("oranges").innerHTML = store.getState().fruit.orange;
+    document.getElementById("sandwitches").innerHTML = store.getState().food.sandwitch;
+}
+store.subscribe(()=>{
+    render();
+})
+
+document.getElementById("purchase-apple").addEventListener('click', ()=>{
     store.dispatch({type: "buy-apple"});
 })
-document.getElementById("apples").innerHTML = store.getState().apple;
+
+document.getElementById("purchase-orange").addEventListener('click', ()=>{
+    store.dispatch({type: "buy-orange"});
+})
+
+document.getElementById("purchase-sandwitch").addEventListener('click', ()=>{
+    store.dispatch({type: "buy-sandwitch"});
+})
+
+render();
