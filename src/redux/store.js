1c2fa6b5781a2d549749f344ce6c314a0286887b
Ali Ghavidel <ghavidel.aliabadi@gmail.com>
2024-01-16 11:43:04 +0330

    part#59

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
-import { legacy_createStore } from 'redux'
+import { applyMiddleware, legacy_createStore } from 'redux'
 import { rootReducer } from './rootReducer';
+import logger from 'redux-logger'
+import { composeWithDevTools } from '@redux-devtools/extension'
 
-const store = legacy_createStore(rootReducer);
+const store = legacy_createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)) );
 
 export default store;
